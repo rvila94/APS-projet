ae250cd9c17c68d43d2996234ab05e92cdd31b42
RODRIGO <21107747@ppti-14-509-09.ufr-info-p6.jussieu.fr>
2025-01-22 12:12:09 +0100

    Ajout syntaxe et grammaire APS0

--- Syntaxe/ML/ast.ml
+++ Syntaxe/ML/ast.ml
@@ -8,16 +8,51 @@
 (* ==  Arbre de syntaxe abstraite                                          == *)
 (* ========================================================================== *)
 
+type typee =
+  ASTBool
+| ASTInt
+| ASTFlech of types * typee
+and types =
+  ASTType of typee
+| ASTTypes of typee * types
+
+type arg = 
+  Arg of string * typee
+and args = 
+  ASTArg of arg
+| ASTArgs of arg * args
 
 type expr =
     ASTNum of int
   | ASTId of string
-  | ASTApp of expr * expr list
+  | ASTIf of expr * expr * expr
+  | ASTAnd of expr * expr
+  | ASTOr of expr * expr
+  | ASTApp of expr * exprs
+  | ASTAbs of args * expr
+and exprs = 
+    ASTExpr of expr
+  | ASTExprs of expr * exprs
 
 type stat =
-    ASTEcho of expr
-      
-type cmd =
+  ASTEcho of expr
+
+type def =
+    ASTConst of string * typee * expr
+  | ASTFun of string * typee * args * expr
+  | ASTFunRec of string * typee * args * expr
+
+type cmds =
     ASTStat of stat
+  | ASTDef of def * cmds
+
+type prog =
+  ASTProg of cmds
+
+
+
+
+
+
 
 	
--- Syntaxe/ML/lexer.mll
+++ Syntaxe/ML/lexer.mll
@@ -18,8 +18,21 @@ rule token = parse
   | '['              { LBRA }
   | ']'              { RBRA }
   | '('              { LPAR }
-  | ')'              { RPAR }
-  | "ECHO"           { ECHO }    
-  | ['0'-'9']+('.'['0'-'9'])? as lxm { NUM(int_of_string lxm) }
-  | ['a'-'z']['a'-'z''A'-'Z''0'-'9']* as lxm { IDENT(lxm) }
+  | ')'              { RPAR }  
+  | ';'              { PVIR }
+  | ','              { VIR }
+  | ':'              { DEUXP }
+  | '*'              { ASTER }
+  | "->"             { FLECH }
+  | "CONST"          { CONST }
+  | "FUN"            { FUN }  
+  | "REC"            { REC }     
+  | "ECHO"           { ECHO }  
+  | "if"             { IF }    
+  | "and"            { AND }  
+  | "or"             { OR }  
+  | "bool"           { BOOL }  
+  | "int"            { INT }  
+  | ('-')?['0'-'9']+ as lxm { NUM(int_of_string lxm) }
+  | ['a'-'z''A'-'Z']['a'-'z''A'-'Z''0'-'9']* as lxm { IDENT(lxm) }
   | eof              { raise Eof }
