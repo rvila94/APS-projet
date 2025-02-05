5148bd31a084b912e4ff61c38cee7022c414ac78
RODRIGO <21107747@ppti-14-509-09.ufr-info-p6.jussieu.fr>
2025-02-05 11:30:02 +0100

    typeur terminé (need tests)

--- Syntaxe/ML/typeur.pl
+++ Syntaxe/ML/typeur.pl
@@ -1,19 +1,109 @@
-bt_prog(prog(Cs)) :-
+extract_typeArgs([], []).
+extract_typeArgs(ARGS, TS) :- 
+    ARGS = [(_, T) | ARGS2],
+    TS = [T | TS2],
+    extract_typeArgs(ARGS2, TS2).
+
+extract_typeExprs(_, [], []).
+extract_typeExprs(G, ES, TS) :- 
+    ES = [E | ES2],
+    TS = [T | TS2], 
+    bt_expr(G, E, T),
+    extract_typeExprs(ES2, TS2).
+
+
+
+% Prog
+bt_prog(prog(CS)) :-
     bt_cmds(G, CS),
     is_init_env(G).
 
 is_init_env(G) :- 
     G = [ ("true", bool)],
     G = [ ("false", bool)],
-    G = [ ("not", astflech([bool]),bool)],
-    G = [ ("eq", astflech([int, int]), bool)],
-    G = [ ("lt", astflech([int, int]), bool)],
-    G = [ ("add", astflech([int, int]), int)],
-    G = [ ("sub", astflech([int, int]), int)],
-    G = [ ("mul", astflech([int, int]), int)],
-    G = [ ("div", astflech([int, int]), int)].
+    G = [ ("not", flech([bool]),bool)],
+    G = [ ("eq", flech([int, int]), bool)],
+    G = [ ("lt", flech([int, int]), bool)],
+    G = [ ("add", flech([int, int]), int)],
+    G = [ ("sub", flech([int, int]), int)],
+    G = [ ("mul", flech([int, int]), int)],
+    G = [ ("div", flech([int, int]), int)].
 
-bt_cmds(_,_).
+% Defs
+bt_cmds(G, def(D, CS)) :-
+    bt_def(G, D, G2),
+    bt_cmds(G2, CS).
 
-bt_expr(G, id(x), T) :- member( (X,T), G ).
+% End
+bt_cmds(G, stat(S)) :-
+    bt_stat(G, S).
 
+% Const
+bt_def(G, const(X, T, E), [(X, T) | G]) :-  
+    bt_expr(G, E, T).
+
+% Fun
+bt_def(G, fun(X, T, ARGS, E), Gfinal) :- 
+    append(ARGS, G, G2),
+    bt_expr(G2, E, T),
+    Gfinal = [(X, Tflech) | G],
+    Tflech = flech(TS, T),
+    extract_typeArgs(ARGS, TS).
+
+% FunRec
+bt_def(G, funRec(X, T, ARGS, E), Gfinal) :- 
+    append(ARGS, G, G2),
+    bt_expr(G3, E, T),
+    Gfinal = [(X, Tflech) | G],
+    Tflech = flech(TS, T),
+    extract_typeArgs(ARGS, TS),
+    G3 = [(X, Tflech) | G2].    % tester de remlacer ca par append((X, Tflech), G2, G3)
+
+% Echo
+bt_stat(G, echo(E)) :-
+    bt_expr(G, E, int).
+
+% Num
+bt_expr(G, num(N), int) :-
+    member((N,int), G ).
+
+% Id
+bt_expr(G, id(X), T) :-
+    member((X,T), G ).
+
+% If
+bt_expr(G, if(E1, E2, E3), T) :-
+    bt_expr(G, E1, bool),
+    bt_expr(G, E2, T),
+    bt_expr(G, E3, T).
+
+% And
+bt_expr(G, and(E1, E2), bool) :-
+    bt_expr(G, E1, bool),
+    bt_expr(G, E2, bool).
+
+% Or
+bt_expr(G, or(E1, E2), bool) :-
+    bt_expr(G, E1, bool),
+    bt_expr(G, E2, bool).
+
+% App [WIP]
+bt_expr(G, app(E, ES), T) :-
+    Tflech = flech(TS, T),
+    bt_expr(G, E, Tflech),
+    extract_typeExprs(G, ES, TS).
+
+% Abs
+bt_expr(G, abs(ARGS, E), Tflech) :-
+    append(ARGS, G, G2),
+    bt_expr(G2, E, T),
+    Tflech = flech(TS, T),
+    extract_typeArgs(ARGS, TS).
+
+% main
+:- 
+    read(P),
+    bt_prog(P).
+
+
+% exemple de commande: ./prologTerm ../Samples/prog0.aps | swipl typeur.pl
