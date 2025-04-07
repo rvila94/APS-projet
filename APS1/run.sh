50c7cf07fc0b8326401474356e5582613b3482f2
RODRIGO <21107747@ppti-14-305-15.ufr-info-p6.jussieu.fr>
2025-04-07 11:20:41 +0200

    Ajout script pour lancer typeur sur tout les fichiers tests

---
+++ APS1/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SAMPLES_DIR="../Samples"
+
+echo "Lancement des tests pour APS1..."
+
+for file in "$SAMPLES_DIR"/*.aps
+do
+    echo "____"
+    echo "Test du fichier " $file ": " 
+    echo "Lancement du typeur..."
+    type_check=$(./prologTerm $file | swipl typeur.pl)
+    echo $type_check
+    if [[ $type_check = *"Type Error"* ]]; then
+        continue
+    fi
+    echo "Lancement de l'evaluateur..."
+    # TODO
+    #res=$(./eval $file)
+    #echo $res
+done
